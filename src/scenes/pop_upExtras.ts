4ccef6556cc8f12aebdd34206f1616d3ac7427bc
axelcastelnovo <renzo.axel.castelnovo@gmail.com>
2021-10-16 19:04:38 -0300

    o

--- src/scenes/pop_upExtras.ts
+++ src/scenes/pop_upExtras.ts
@@ -19,13 +19,7 @@ export default class pop_upExt extends Phaser.Scene
     const fondoPopUpExtras = this.add.image(680, 425, 'botonNivel').setScale(1.1);
     const buttonAtras = this.add.image(1000, 150, 'botonatras').setScale(0.8)
     .setInteractive()
-    .on('pointerdown', () => this.scene.start('menuPpal') && this.scene.start('extras'));
-
-    
-
-    
-
-    
+    .on('pointerdown', () => this.scene.start('extras'));
   }  
 
   public mostrarInfo(info:string)
@@ -34,13 +28,33 @@ export default class pop_upExt extends Phaser.Scene
     {
       const tituloPopUpY= this.add.text(525, 130, 'YAGUARETÉ', {font: 'bold 40pt Arial', align:'center', color: 'black'}).setDepth(3);
 
-      const fotoFamiliarY = this.add.image(350, 435, 'fotoYaguarete').setScale(0.8).setDepth(3);
+      const fotoFamiliarY = this.add.image(350, 435, 'fotoYaguarete').setScale(0.8).setDepth(4);
 
       const txtYagua = this.add.text(500, 250, 'El nombre yaguareté es de origen guaraní y \nsignifica “la verdadera fiera”. Este gato, cuyo \nnombre científico es Panthera onca, es el felino \nmás grande del continente americano y tercero \nen corpulencia a escala mundial, después del tigre \nde bengala y el león. Se destaca por su aspecto \nrobusto y la cabeza proporcionalmente grande con \npoderosa estructura mandibular. Los machos \nalcanzan los 2,50 metros de longitud, incluida la \ncola, y hasta 140 kg de peso. Además el yaguareté \nes un excelente nadador, un caminador incansable, \nsolitario y muy territorial.', {font: 'bold 20pt Arial', color: 'black'}).setDepth(3);
     } 
-    if (info='mono')
+    
+    if (info='monoInformation')
     {
-      const txtYagua = this.add.text(500, 250, 'Hola Mono', {font: 'bold 20pt Arial', color: 'black'});
+      const tituloPopUpY= this.add.text(525, 130, 'MONO', {font: 'bold 40pt Arial', align:'center', color: 'black'}).setDepth(3);
+      const txtYagua = this.add.text(500, 250, 'Hola Mono', {font: 'bold 20pt Arial', color: 'black'}).setDepth(3);
+    }
+    
+    if (info='condorInformation')
+    {
+      const tituloPopUpY= this.add.text(525, 130, 'CONDOR', {font: 'bold 40pt Arial', align:'center', color: 'black'}).setDepth(3);
+      const txtYagua = this.add.text(500, 250, 'Hola Condor', {font: 'bold 20pt Arial', color: 'black'}).setDepth(3);
+    }
+    
+    if (info='ballenaInformation')
+    {
+      const tituloPopUpY= this.add.text(525, 130, 'BALLENA', {font: 'bold 40pt Arial', align:'center', color: 'black'}).setDepth(3);
+      const txtYagua = this.add.text(500, 250, 'Hola Ballena', {font: 'bold 20pt Arial', color: 'black'}).setDepth(3);
+    }
+    
+    if (info='pinguinoInformation')
+    {
+      const tituloPopUpY= this.add.text(525, 130, 'PINGÜINO', {font: 'bold 40pt Arial', align:'center', color: 'black'}).setDepth(3);
+      const txtYagua = this.add.text(500, 250, 'Hola Pingüino', {font: 'bold 20pt Arial', color: 'black'}).setDepth(3);
     }
   }
 
